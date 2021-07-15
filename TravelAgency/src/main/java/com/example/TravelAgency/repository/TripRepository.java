570d887cd491ecb5acdf59d4e53aee8ac0bdc98b
Kristi <kristi.martiko@fshnstudent.info>
2021-07-15 23:46:43 +0200

    Updated

--- TravelAgency/src/main/java/com/example/TravelAgency/repository/TripRepository.java
+++ TravelAgency/src/main/java/com/example/TravelAgency/repository/TripRepository.java
@@ -1,30 +1,20 @@
 package com.example.TravelAgency.repository;
 
-import com.example.TravelAgency.model.Trip;
+import com.example.TravelAgency.enumeration.TripStatusEnum;
+import com.example.TravelAgency.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
-    @Query(value = "SELECT * FROM trip WHERE status = 'Created'", nativeQuery = true)
-    public List<Trip> tripsCreated();
-
-    @Query(value = "SELECT * FROM trip WHERE status_id = 'Waiting For Approval'", nativeQuery = true)
-    public List<Trip> tripsSendForApproval();
-
-    @Query(value = "SELECT * FROM trip WHERE status_id = 'Approved'", nativeQuery = true)
-    public List<Trip> tripsApproved();
-
-    @Query(value = "SELECT * FROM trip WHERE trip_id = :trip_id", nativeQuery = true)
-    public Trip isPresent(@Param("trip_id") Long trip_id);
-
-    @Query(value = "UPDATE trip SET status_id = 'Waiting For Approval' WHERE trip_id = :trip_id", nativeQuery = true)
-    public void sendApprove(@Param("trip_id") Long trip_id);
-
-    @Query(value = "UPDATE trip SET sataus_id = 'Approved' WHERE trip_id = :trip_id", nativeQuery = true)
-    public void approve(@Param("trip_id") Long trip_id);
 
     public void deleteTripById(@Param("trip_id") Long trip_id);
+
+    public List<Trip> findAllByStatusIs(TripStatusEnum tripStatusEnum);
+
+    @Query(value = "SELECT status from Trip  WHERE trip_id :trip_id", nativeQuery = true)
+    public String getStatus(@Param("trip_id") Long tripId);
 }
