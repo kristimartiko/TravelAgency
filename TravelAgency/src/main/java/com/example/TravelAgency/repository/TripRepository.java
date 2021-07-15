6a29abc21a50c9e404cac336e3800cb237186b7b
Kristi <kristi.martiko@fshnstudent.info>
2021-07-16 00:05:52 +0200

    Update

--- TravelAgency/src/main/java/com/example/TravelAgency/repository/TripRepository.java
+++ TravelAgency/src/main/java/com/example/TravelAgency/repository/TripRepository.java
@@ -1,19 +1,18 @@
 package com.example.TravelAgency.repository;
 
 import com.example.TravelAgency.enumeration.TripStatusEnum;
-import com.example.TravelAgency.entity.Trip;
+import com.example.TravelAgency.entity.TripEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
-public interface TripRepository extends JpaRepository<Trip, Long> {
+public interface TripRepository extends JpaRepository<TripEntity, Long> {
 
     public void deleteTripById(@Param("trip_id") Long trip_id);
 
-    public List<Trip> findAllByStatusIs(TripStatusEnum tripStatusEnum);
+    public List<TripEntity> findAllByStatusIs(TripStatusEnum tripStatusEnum);
 
     @Query(value = "SELECT status from Trip  WHERE trip_id :trip_id", nativeQuery = true)
     public String getStatus(@Param("trip_id") Long tripId);
