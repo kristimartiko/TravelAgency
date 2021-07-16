bf85f80e500d725e53802cde1380533469a9472d
Kristi <kristi.martiko@fshnstudent.info>
2021-07-16 15:31:53 +0200

    User Details Added

---
+++ TravelAgency/src/main/java/com/example/TravelAgency/service/MyUserDetailService.java
@@ -0,0 +1,47 @@
+package com.example.TravelAgency.service;
+
+import com.example.TravelAgency.entity.RoleEntity;
+import com.example.TravelAgency.entity.UserEntity;
+import com.example.TravelAgency.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findUserByEmail(s);
+        if(userEntity == null) {}
+
+        return new org.springframework.security.core.userdetails.
+                User(userEntity.getEmail(), userEntity.getPassword(),mapRolesToAuthorities(userEntity.getRoleEntities()));
+    }
+
+    public String getCurrentUserName(String email) {
+        UserEntity userEntity = userRepository.findUserByEmail(email);
+        return userEntity.getFirstName();
+    }
+
+    public UserEntity getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findUserByEmail(auth.getName());
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<RoleEntity> roleEntities) {
+        return roleEntities.stream().map(roleEntity -> new SimpleGrantedAuthority(roleEntity.getName())).collect(Collectors.toList());
+    }
+}
