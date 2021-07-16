66d6907b9d95d82106b98d87d6ef5b8cef11c3c4
Kristi <kristi.martiko@fshnstudent.info>
2021-07-16 15:57:34 +0200

    Update

--- TravelAgency/src/main/java/com/example/TravelAgency/repository/UserRepository.java
+++ TravelAgency/src/main/java/com/example/TravelAgency/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.TravelAgency.repository;
 
-import com.example.TravelAgency.model.User;
+import com.example.TravelAgency.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findUserByEmail(@Param("email") String value);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    public UserEntity findUserByEmail(@Param("email") String value);
 }
