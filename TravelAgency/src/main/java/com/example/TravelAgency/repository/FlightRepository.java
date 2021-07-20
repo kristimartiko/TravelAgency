5f2d5e097a41f3c35435432cc405f639425fed62
Kristi <kristi.martiko@fshnstudent.info>
2021-07-20 16:16:56 +0200

    Added Delete Flight By Trip

--- TravelAgency/src/main/java/com/example/TravelAgency/repository/FlightRepository.java
+++ TravelAgency/src/main/java/com/example/TravelAgency/repository/FlightRepository.java
@@ -4,6 +4,8 @@ import com.example.TravelAgency.entity.FlightEntity;
 import com.example.TravelAgency.entity.TripEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,5 +14,6 @@ public interface FlightRepository extends JpaRepository<FlightEntity, Long> {
     public List<FlightEntity> findFlightEntityByTripEntity(TripEntity tripEntity);
 
     @Modifying
-    public void deleteAllByTripEntity(TripEntity tripEntity);
+    @Query(value = "DELETE FROM flight WHERE trip_id = :trip_id", nativeQuery = true)
+    public void deleteAllByTripEntity(@Param("trip_id") Long trip_id);
 }
