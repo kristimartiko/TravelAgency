601e2fb9ede326fd8a3d5c72dd2ec8df3bb7cc41
Kristi <kristi.martiko@fshnstudent.info>
2021-07-21 16:04:00 +0200

    Roles Mapped

--- TravelAgency/src/main/java/com/example/TravelAgency/service/MyUserDetailService.java
+++ TravelAgency/src/main/java/com/example/TravelAgency/service/MyUserDetailService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,6 +43,10 @@ public class MyUserDetailService implements UserDetailsService {
     }
 
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<RoleEntity> roleEntities) {
-        return roleEntities.stream().map(roleEntity -> new SimpleGrantedAuthority(roleEntity.getName())).collect(Collectors.toList());
+        Collection<GrantedAuthority> authorities = new HashSet<>();
+        for(RoleEntity role: roleEntities) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
     }
 }
